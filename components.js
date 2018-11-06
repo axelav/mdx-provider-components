72b45155f86c115346a0bf461e652a9d60e2fa41
Axel Anderson <axel@ftml.net>
2018-11-06 12:41:07 -0500

    initial commit

---
+++ components.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { widont } from 'journalize'
+
+const headingClassNames = 'mt0 mb4 hl-title'
+
+const Heading = ({ children }) => (
+  <h1 className={`${headingClassNames} f3`}>{children}</h1>
+)
+const Subheading = ({ children }) => (
+  <h2 className={`${headingClassNames} f4`}>{children}</h2>
+)
+const Title = ({ children }) => (
+  <h3 className={`${headingClassNames} f5`}>{children}</h3>
+)
+const Subtitle = ({ children }) => (
+  <h4 className={`${headingClassNames} f5 i normal`}>{children}</h4>
+)
+const Text = ({ children }) => {
+  if (typeof children === 'string') {
+    return <p className="lh-copy mt0 mb4 measure-wide-ns">{widont(children)}</p>
+  }
+
+  return <p className="lh-copy mt0 mb4 measure-wide-ns">{children}</p>
+}
+const Link = ({ children, href }) => (
+  <a className="blue hover-dark-blue link" href={href}>
+    {children}
+  </a>
+)
+// TODO use components/Image here
+const Image = ({ src }) => <img className="db mw-100 mb4" src={src} />
+const Blockquote = ({ children }) => (
+  <blockquote className="mt0 mh0 mb4 ph3 i bl b--light-gray">
+    {children}
+  </blockquote>
+)
+const Pre = ({ children }) => (
+  <pre className="pre bg-near-white pa3">{children}</pre>
+)
+const Code = ({ children }) => <code>{children}</code>
+const List = ({ children }) => <ul className="mt0 mh0 mb4 pl4">{children}</ul>
+const Item = ({ children }) => <li className="mt0 mb2 lh-copy">{children}</li>
+const Table = ({ children }) => (
+  <table className="collapse mb4 ba b--light-gray">{children}</table>
+)
+
+export {
+  Heading,
+  Subheading,
+  Title,
+  Subtitle,
+  Text,
+  Link,
+  Image,
+  Blockquote,
+  Pre,
+  Code,
+  List,
+  Item,
+  Table
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {
+  Heading,
+  Subheading,
+  Title,
+  Subtitle,
+  Text,
+  Link,
+  Image,
+  Blockquote,
+  Pre,
+  Code,
+  List,
+  Item,
+  Table
+} from './components'
+
+const createComponent = (Component, defaultProps) => {
+  const C = props => React.createElement(Component, props)
+  C.defaultProps = defaultProps
+  return C
+}
+
+const createHeading = Component => props => {
+  if (!props.id) {
+    return React.createElement(Component, props)
+  }
+
+  return React.createElement(
+    Component,
+    props,
+    React.createElement(
+      Link,
+      {
+        href: '#' + props.id
+      },
+      props.children
+    )
+  )
+}
+
+const createScope = (props = {}) => {
+  const commonComponents = {
+    h1: createHeading(Heading),
+    h2: createHeading(Subheading),
+    h3: createHeading(Title),
+    h4: createHeading(Subtitle),
+    // h5: createHeading(Heading),
+    // h6: createHeading(Heading),
+    p: Text,
+    a: Link,
+    img: Image,
+    blockquote: Blockquote,
+    pre: Pre,
+    code: Code,
+    inlineCode: Code,
+    ul: List,
+    li: Item,
+    table: Table
+  }
+
+  const tags = Object.keys(commonComponents)
+
+  return tags.reduce((scope, tag) => {
+    const Component = commonComponents[tag]
+
+    scope[tag] = createComponent(Component, props)
+    return scope
+  }, {})
+}
+
+export default createScope
